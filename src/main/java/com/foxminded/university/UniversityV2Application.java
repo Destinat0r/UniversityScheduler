24eacb28c38da7f1e78cd68ab23a35e7af56eef8
Rubezh <ashavrn@protonmail.com>
2019-04-11 14:12:24 +0300

    Spring Boot project starter created

---
+++ src/main/java/com/foxminded/university/UniversityV2Application.java
@@ -0,0 +1,13 @@
+package com.foxminded.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityV2Application.class, args);
+	}
+
+}
