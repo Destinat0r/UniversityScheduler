6b4cef4d575a4a5626f55d9a71f30f4f594be66f
Rubezh <ashavrn@protonmail.com>
2019-04-12 15:02:43 +0300

    added reading all students to front-end, added 'create' form to create a new student

--- src/main/java/com/foxminded/university/view/MainController.java
+++ src/main/java/com/foxminded/university/view/MainController.java
@@ -4,18 +4,25 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.foxminded.university.dao.StudentRepository;
+import com.foxminded.university.domain.Group;
 import com.foxminded.university.domain.Student;
+import com.foxminded.university.service.impl.GroupService;
+import com.foxminded.university.service.impl.StudentService;
 
 @Controller
 public class MainController {
     
-    private StudentRepository studentRepository;
+    private StudentService studentService;
+    private GroupService groupService;
     
-    public MainController(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
+    public MainController(StudentService studentService, GroupService groupService) {
+        this.studentService = studentService;
+        this.groupService = groupService;
     }
 
     @RequestMapping("/")
@@ -23,13 +30,30 @@ public class MainController {
         return "index";
     }
     
-    @RequestMapping("/students")
+    @GetMapping("/students")
     public String allStudents(Model model) {
         
-        List<Student> students = studentRepository.findAll();
+        List<Student> students = studentService.findAll();
+        List<Group> groups = groupService.findAll();
+        
+        System.out.println(groups);
+        
+        Student student = new Student();
+        Group group = new Group();
+        
+        model.addAttribute("student", student);
         model.addAttribute("students", students);
+        model.addAttribute("groups", groups);
+        model.addAttribute("group", group);
         
         return "/students/all_students";
+    }
+    
+    @PostMapping("/students")
+    public String createStudent(@ModelAttribute("student") Student student) {
+        
+        studentService.create(student);
+        return "redirect:/students";
     }
     
 }
